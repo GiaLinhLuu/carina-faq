4c04acc127e8ab98a28eb5fb18935cd22d517ec6
TranPhat-28 <tphat8301@gmail.com>
2026-06-27 17:29:17 +0700

    Install TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-    return <div>Hello it's Carina</div>;
+    return (
+        <div className="bg-blue-800 text-white h-full w-full flex items-center justify-center">
+            Hello it's Carina
+        </div>
+    );
 }
 
 export default App;
