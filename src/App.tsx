a7605f063c18ecbfb4b3c1d1e48e3d311464391f
TranPhat-28 <tphat8301@gmail.com>
2026-06-27 17:33:44 +0700

    Install Daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
     return (
         <div className="bg-blue-800 text-white h-full w-full flex items-center justify-center">
             Hello it's Carina
+            <button className="btn btn-warning">Warning</button>
         </div>
     );
 }
