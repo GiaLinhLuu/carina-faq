b6cc41da3bef0a43bd5a1b83a7af722c0ed82c48
TranPhat-28 <tphat8301@gmail.com>
2026-06-29 22:39:52 +0700

    Minor style change

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,12 +2,13 @@ import { TypeAnimation } from "react-type-animation";
 
 export const Hero = () => {
     return (
-        <section className="w-full h-1/5 flex flex-col items-center justify-center max-w-4xl sticky top-0 z-10 pb-4">
-            <div className="flex items-center mb-1md:mb-2 lg:mb-3 p-4 font-bold text-4xl md:text-5xl lg:text-6xl">
+        <section className="w-full h-fit flex flex-col items-center justify-center max-w-4xl sticky top-0 z-10 pb-4">
+            <div className="flex items-center mb-1md:mb-2 lg:mb-3 p-4 font-bold text-3xl md:text-4xl lg:text-5xl">
                 <p>
                     Hello it's{" "}
+                    <span className="text-primary-content">Carina</span> from{" "}
                     <span className="bg-primary rounded-lg px-2 text-primary-content">
-                        Carina!
+                        Accounting Team!
                     </span>
                 </p>
             </div>
