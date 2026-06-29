055c9c5bf1df216856f64369a46dce740dedd425
TranPhat-28 <tphat8301@gmail.com>
2026-06-30 01:29:18 +0700

    Fixed width for column

--- src/components/TableRenderer/TableRenderer.tsx
+++ src/components/TableRenderer/TableRenderer.tsx
@@ -3,16 +3,22 @@ import type { TableItem } from "../../types/types";
 import { ContentDisplayer } from "../ContentDisplayer/ContentDisplayer";
 
 export const TableRenderer = ({ table }: { table: TableItem }) => {
+    /** Data is guaranteed to to be 2 columns */
     return (
         <div
             className={`overflow-x-auto ${table.styles?.join(" ") || ""} rounded-box border border-base-content/5 bg-base-100`}
         >
-            <table className={"table"}>
+            <table className={"table w-full"}>
                 {table.header && (
                     <thead className="bg-primary">
                         <tr>
-                            {table.header.cells.map((cell) => (
-                                <th key={cell.id}>
+                            {table.header.cells.map((cell, index) => (
+                                <th
+                                    key={cell.id}
+                                    className={
+                                        index === 0 ? "w-[60%]" : "w-[40%]"
+                                    }
+                                >
                                     <ReactMarkdown
                                         components={{
                                             p: ({ children }) => (
